efe0e179fece2245300c6fd26883f8309c9cf0aa
kseroff <112875767+kseroff@users.noreply.github.com>
2025-08-26 22:22:05 +0300

    Add files via upload

---
+++ laba OC 5/Dll2/dllmain.cpp
@@ -0,0 +1,54 @@
+﻿#include "pch.h"
+#include <windows.h>
+#include <tchar.h>
+#include <psapi.h>
+
+#pragma data_seg(".datashr")
+DWORD dwProcessesAttached = 0;
+DWORD dwAllThreadsAttached = 0;
+#pragma data_seg() 
+#pragma comment(linker, "/SECTION:.datashr,RWS") 
+
+DWORD dwThreadsAttached = 0;
+__declspec(dllexport) int i = 0;
+
+// Функция для отправки статистики по использованию памяти
+__declspec(dllexport) int SendMemoryUsage(char* nameDLL) {
+
+	PROCESS_MEMORY_COUNTERS_EX pmc;
+	pmc.cb = sizeof(pmc);
+	GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+
+	TCHAR message[256];
+	//wsprintf(message, _T("\n There are %s libraries in use \n Memory Usage: %lu KB \n"), nameDLL, (pmc.WorkingSetSize / 1024));
+	return (pmc.WorkingSetSize / 1024);
+}
+
+// Функция DllMain
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		++dwProcessesAttached;
+		++dwAllThreadsAttached;
+		++dwThreadsAttached;
+		break;
+
+	case DLL_THREAD_ATTACH:
+		++dwAllThreadsAttached;
+		++dwThreadsAttached;
+		break;
+
+	case DLL_THREAD_DETACH:
+		--dwAllThreadsAttached;
+		--dwThreadsAttached;
+		break;
+
+	case DLL_PROCESS_DETACH:
+		--dwProcessesAttached;
+		--dwAllThreadsAttached;
+		--dwThreadsAttached;
+		break;
+	}
+	return TRUE;
+}
---
+++ laba OC 5/Laba 5 OC/Laba 5 OC.cpp
@@ -0,0 +1,42 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <tchar.h>
+
+typedef int (*SendMemory)(char*);
+
+__declspec(dllimport) int SendMemoryUsage_(char* nameDLL);
+
+int _tmain() {
+
+    char temp[] = { "1 labraries" };
+    printf(("\n There are %s libraries in use \n Memory Usage: %lu KB \n"), temp, SendMemoryUsage_(temp));
+
+    //////////////////////////////////////////////////////////
+
+    HINSTANCE hDLL = LoadLibrary(L"Dll2.dll");
+    if (hDLL == NULL) {
+        std::cerr << "Failed to load DLL." << std::endl;
+        return 1;
+    }
+
+    // Получение указателя на функцию
+
+    SendMemory addFunc = (SendMemory)GetProcAddress(hDLL, "SendMemoryUsage");
+
+    if (addFunc == NULL) {
+        std::cerr << "Failed to get function pointer." << std::endl;
+        FreeLibrary(hDLL);
+        return 1;
+    }
+
+    // Вызов функции
+    char temp2[] = { "2 labraries" };
+    //std::cout<<std::endl<<addFunc(temp2);
+    printf(("\n There are %s libraries in use \n Memory Usage: %lu KB \n"), temp2, addFunc(temp2));
+
+    // Освобождение первой DLL
+    FreeLibrary(hDLL);
+
+
+    return 0;
+}
